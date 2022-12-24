08b190fd35d9d2accc8d4a87d87577b6c5f615eb
이원문 <ww10104@naver.com>
2022-12-24 16:41:28 +0900

    feat: login signup createagit page 레이아웃 추가

---
+++ src/pages/CreateAgit.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import CreateAgitForm from "../components/CreateAgit/CreateAgitForm";
+
+const CreateAgit = () => {
+  return (
+    <StDiv>
+      <CreateAgitForm />
+    </StDiv>
+    )
+};
+
+const StDiv = styled.div`
+  color: var(--color-point-blue);
+  width: 1200px;
+  height: 700px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export default CreateAgit;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,3 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import SignUpForm from "../components/SignUp/SignUpForm";
+
+const SignUp = () => {
+  return (
+    <StDiv>
+      <SignUpForm/>
+    </StDiv>
+    )
+};
+
+const StDiv = styled.div`
+  color: var(--color-point-blue);
+  width: 1200px;
+  height: 700px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export default SignUp;
+
+
 // import React from "react";
 // import styled from "styled-components";
 // import SignUpForm from "../components/signUp/SignUpForm";
