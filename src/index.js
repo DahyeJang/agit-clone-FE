80098d0acb8f199fc3f188ccfb9d1e3301dceb8a
DahyeJang <everything888@naver.com>
2022-12-26 14:53:58 +0900

    fix: provider 하나 삭제

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import reportWebVitals from './reportWebVitals'
+import reportWebVitals from "./reportWebVitals";
 import store from "./redux/config/configStore";
-import { Provider } from "react-redux";
 //import { CookiesProvider } from "react-cookie";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
