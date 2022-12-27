8da896fa21f82cc91ac6aa3fcd43edaf47ff9884
이원문 <ww10104@naver.com>
2022-12-27 10:25:08 +0900

    feat: 회원가입 기능구현, 로그인 기능 진행중

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,30 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { baseURL } from "../../core/api/axios";
+import { setCookie } from "../../core/api/cookie";
 
 const LoginForm = () => {
-
   const navigate = useNavigate();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const signUpBtn = () => {
-    navigate("/SignUp")
-  }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (username === "" || password === "") {
+      alert("아이디, 비밀번호를 확인해주세요.");
+    } else {
+      postLogin({
+        username,
+        password,
+      }).then((res) => {
+        if (res === undefined) {
+          navigate(`/login`);
+        } else {
+          navigate(`/`);
+          setCookie("id", res.headers.authorization);
+        }
+      });
+    }
+  };
+
+  const postLogin = async (post) => {
+    try {
+      const data = await baseURL.post("/user/login", post);
+      if (data.data.statusCode === 200) {
+        return data;
+      } else {
+        alert("아이디, 비밀번호를 확인해주세요.");
+      }
+    } catch (error) {}
+  };
+
   return (
     <div>
       <Login>아지트 로그인</Login>
       <StDiv>
-       <StInput placeholder="아이디"></StInput>
-    </StDiv>
-    <StDiv>
-    <StInput placeholder="비밀번호"></StInput>
-    </StDiv>
-    <StDiv>
-    <BtnDiv><EndButton>다음</EndButton>
-      <EndButton onClick={signUpBtn}>회원가입</EndButton></BtnDiv>
-    </StDiv>
+        <StInput
+          name="username"
+          type="text"
+          placeholder="아이디"
+          onChange={(e) => {
+            const { value } = e.target;
+            setUserName(value);
+          }}
+        ></StInput>
+      </StDiv>
+      <StDiv>
+        <StInput
+          name="password"
+          type="password"
+          placeholder="비밀번호"
+          onChange={(e) => {
+            const { value } = e.target;
+            setPassword(value);
+          }}
+        ></StInput>
+      </StDiv>
+      <StDiv>
+        <BtnDiv>
+          <EndButton
+            onClick={(e) => {
+              onSubmitHandler(e);
+            }}
+          >
+            다음
+          </EndButton>
+          <EndButton
+            onClick={() => {
+              navigate("/signup");
+            }}
+          >
+            회원가입
+          </EndButton>
+        </BtnDiv>
+      </StDiv>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
 
@@ -36,11 +96,11 @@ const Login = styled.div`
   margin-bottom: 20px;
   font-size: 35px;
   color: black;
-`
+`;
 
 const StDiv = styled.div`
   margin-bottom: 10px;
-`
+`;
 
 const StInput = styled.input`
   width: 482px;
@@ -48,11 +108,11 @@ const StInput = styled.input`
   padding: 0px 0px 0px 20px;
   font-size: medium;
   border: 1px solid #e2e2e2;
-`
+`;
 
 const EndButton = styled.button`
   color: white;
-  width:250px;
+  width: 250px;
   height: 60px;
   font-size: medium;
   border: 1px solid var(--color-point-blue);
@@ -67,9 +127,9 @@ const EndButton = styled.button`
   &:focus {
     outline: none;
   }
-`
+`;
 
 const BtnDiv = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from './reportWebVitals'
 import store from "./redux/config/configStore";
-import { Provider } from "react-redux";
 //import { CookiesProvider } from "react-cookie";
 
 
